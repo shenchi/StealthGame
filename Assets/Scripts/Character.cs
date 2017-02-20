56a1fcaf858bb52fa227f4f22ca7c5a228a1dad0
shenchi <shenchi710@gmail.com>
2017-02-20 15:07:17 -0500

    first commit

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Character : MonoBehaviour
+{
+    public float speed = 0;
+
+    //[SerializeField]
+    private Animator animator;
+    private int speedId;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        speedId = Animator.StringToHash("Speed");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat(speedId, speed);
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Character))]
+public class PlayerInput : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
